e502a51ff3c01b164362959c14ebc2c0ab9bc706
Pablo Maceda <pablomaceda@Pablos-MacBook-Pro.local>
2019-04-30 20:17:50 -0500

    Checking work

--- deep_learning2.0/CFModel.py
+++ deep_learning2.0/CFModel.py
@@ -3,17 +3,17 @@
 # Keras uses TensorFlow tensor library as the backend system to do the heavy compiling
 
 import numpy as np
-from keras.layers import Input, Embedding, Concatenate
-from keras.models import Model
+from keras.layers import Input, Embedding, dot, Dense, Reshape
+from keras.models import Model, Sequential
 
-class CFModel(Sequential):
+class CFModel(Model):
 
     # The constructor for the class
     def __init__(self, n_users, m_items, k_factors, **kwargs):
         # P is the embedding layer that creates an User by latent factors matrix.
         # If the intput is a user_id, P returns the latent factor vector for that user.
         user_input = Input(shape=(1,), name='user_input')
-        x = Embedding(input_dim=n_users output_dim=k_factors, input_length=1)(user_input)
+        x = Embedding(input_dim=n_users, output_dim=k_factors, input_length=1)(user_input)
         user_output = Reshape((k_factors,))(x)
 
         # Q is the embedding layer that creates a Movie by latent factors matrix.
@@ -21,11 +21,11 @@ class CFModel(Sequential):
         movie_input = Input(shape=(1,), name='movie_input')
         y = Embedding(input_dim=m_items, output_dim=k_factors, input_length=1)(movie_input)
         movie_output = Reshape((k_factors,))(y)
-
-        super(CFModel, self).__init__(**kwargs)
         
         # The Merge layer takes the dot product of user and movie latent factor vectors to return the corresponding rating.
-        self.add(Merge([P, Q], mode='dot', dot_axes=1))
+        z = dot([Dense(10)(user_output), Dense(10)(movie_output)], axes=1)
+
+        self.Model(inputs=[user_input, movie_input], outputs=z)
 
     # The rate function to predict user's rating of unrated items
     def rate(self, user_id, item_id):
